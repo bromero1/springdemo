9dd7cb9a69b6281d7c1ad8ca4339ef3acd159a16
Bryan Romero <bromero52@gmail.com>
2023-12-20 14:00:24 -0500

    Disable csrf for dev

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,19 +1,27 @@
-package com.example.demo.config;
+package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // TODO: 12/20/23 Enable csrf on production.
+    // https://www.baeldung.com/spring-security-csrf#stateless-spring-api
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests().anyRequest();
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+
 
         return http.build();
     }
