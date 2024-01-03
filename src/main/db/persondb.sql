cc3cd4d519f48ab6f44b23d9cdcd913d152c9d77
Bryan Romero <bromero52@gmail.com>
2024-01-03 12:13:54 -0500

    Add first/last name to schema

--- src/main/db/persondb.sql
+++ src/main/db/persondb.sql
@@ -7,15 +7,15 @@ create table person (
         dob date,
         email varchar(255),
         name varchar(255),
-        primary key (person_id)
+        primary key (person_id),
+        unique(email)
     );
     
     
+insert into person(age, dob, email, first_name, last_name) values (29, '1990-01-17', 'roero5@gmail.com', 'Jeff', 'Romero');
+insert into person(age, dob, email, first_name, last_name) values (25, '1999-02-08', 'test@gmail.com', 'John', 'Wick');
+insert into person(age, dob, email, first_name, last_name) values (24, '1984-03-29', 'tewst@gmail.com', 'Aretha', 'Franklin');
+insert into person(age, dob, email, first_name, last_name) values (88, '1940-03-21', 'oldie@gmail.com', 'Frank', 'Tank');
+
 select * from person;
-insert into person(age, dob, email, name) values (29, '1994-03-27', 'bromero52@gmail.com', 'brdyan');
-insert into person(age, dob, email, name) values (25, '1994-03-27', 'bromerdfso52@gmail.com', 'brydan');
-insert into person(age, dob, email, name) values (24, '1994-03-27', 'bromero52@gmail.com', 'bryadan');
-insert into person(age, dob, email, name) values (88, '1940-03-27', 'oldie@gmail.com', 'Frank');
-
-
 -- delete from person where person_id = 9;
--- src/main/java/com/example/demo/person/Person.java
+++ src/main/java/com/example/demo/person/Person.java
@@ -1,36 +1,37 @@
 package com.example.demo.person;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
 @Table
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode @ToString
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long person_id;
-    private String name;
+    private String first_name;
 
+    private String last_name;
     @Column(unique=true)
     private String email;
     private LocalDate dob;
     private Integer age;
 
-    public Person() {
-    }
-    public Person(Long person_id, String name, String email, LocalDate dob, Integer age) {
+    public Person(Long person_id, String first_name, String email, LocalDate dob, Integer age) {
         this.person_id = person_id;
-        this.name = name;
+        this.first_name = first_name;
         this.email = email;
         this.dob = dob;
         this.age = age;
     }
 
-    public Person(String name, String email, LocalDate dob, Integer age) {
-        this.name = name;
+    public Person(String first_name, String email, LocalDate dob, Integer age) {
+        this.first_name = first_name;
         this.email = email;
         this.dob = dob;
         this.age = age;
@@ -46,12 +47,12 @@ public class Person {
         this.person_id = person_id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
     }
 
     public String getEmail() {
@@ -77,28 +78,5 @@ public class Person {
     public void setAge(Integer age) {
         this.age = age;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return Objects.equals(person_id, person.person_id) && Objects.equals(name, person.name) && Objects.equals(email, person.email) && Objects.equals(dob, person.dob) && Objects.equals(age, person.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(person_id, name, email, dob, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "person_id=" + person_id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
-    }
+    
 }
