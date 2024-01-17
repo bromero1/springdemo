7952032d9a58ca58a8bf3512d167126b2902f953
Bryan Romero <bromero52@gmail.com>
2024-01-17 17:28:45 -0500

    oauth controller and config

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function Login() {
 
     const handleGoogleLogin = async () => {
-        var endpoint = "/auth/google";
+        var endpoint = "localhost:8080/auth/google";
         try {
             const response = await axios.get(endpoint);
             window.location.href = response.data.redirectUrl; 
