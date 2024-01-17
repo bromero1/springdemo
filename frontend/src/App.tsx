d2a643a086e132de5970bed1cfc2ed6f69148606
Bryan Romero <bromero52@gmail.com>
2024-01-16 19:17:57 -0500

    route for login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import JournalEntry from "./pages/JournalEntry";
 import Home from "./pages/Home";
+//import OAuth2Redir from "./user/oauth2/OAuth2Redir";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        {/* <Route path="/oauth2/redirect" element={<OAuth2Redir />}></Route> */}
         <Route path="/daily" element={<JournalEntry />} />
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
-
   );
 }
 
