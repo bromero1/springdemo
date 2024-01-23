4c2783f98733eba0a4e11019628f873157fe5cb4
Bryan Romero <bromero52@gmail.com>
2024-01-23 16:55:41 -0500

    Temp

--- src/main/java/com/example/demo/security/OAuth2Config.java
+++ src/main/java/com/example/demo/security/OAuth2Config.java
@@ -1,43 +1,43 @@
-package com.example.demo.security;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
-
-//@Configuration
-public class OAuth2Config {
-
-    @Value("${spring.security.oauth2.client.registration.google.client-id}")
-    private String googleClientId;
-
-    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
-    private String googleClientSecret;
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(){
-        return new InMemoryClientRegistrationRepository();
-    }
-
-    private ClientRegistration getGoogleClientRegistration(){
-        return ClientRegistration.withRegistrationId("google")
-                .clientId(googleClientId)
-                .clientSecret(googleClientSecret)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("/login/oauth2/code/{registrationId}")
-                .scope("openid", "profile", "email")
-                .authorizationUri("https://accounts.google")
-                .tokenUri("https://www.googleapis.com/oauth2/v3/token")
-                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-                .userNameAttributeName(IdTokenClaimNames.SUB)
-                .clientName("Google")
-                .build();
-    }
-
-}
+//package com.example.demo.security;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.oauth2.client.registration.ClientRegistration;
+//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+//import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+//import org.springframework.security.oauth2.core.AuthorizationGrantType;
+//import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+//import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+//
+////@Configuration
+//public class OAuth2Config {
+//
+//    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+//    private String googleClientId;
+//
+//    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+//    private String googleClientSecret;
+//
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository(){
+//        return new InMemoryClientRegistrationRepository();
+//    }
+//
+//    private ClientRegistration getGoogleClientRegistration(){
+//        return ClientRegistration.withRegistrationId("google")
+//                .clientId(googleClientId)
+//                .clientSecret(googleClientSecret)
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .redirectUri("/login/oauth2/code/{registrationId}")
+//                .scope("openid", "profile", "email")
+//                .authorizationUri("https://accounts.google")
+//                .tokenUri("https://www.googleapis.com/oauth2/v3/token")
+//                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+//                .userNameAttributeName(IdTokenClaimNames.SUB)
+//                .clientName("Google")
+//                .build();
+//    }
+//
+//}
--- src/main/java/com/example/demo/security/OAuth2Controller.java
+++ src/main/java/com/example/demo/security/OAuth2Controller.java
@@ -1,19 +1,19 @@
-package com.example.demo.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class OAuth2Controller {
-    private final OAuth2AuthorizedClientService clientService;
-
-    @Autowired
-    public OAuth2Controller(OAuth2AuthorizedClientService clientService){
-        this.clientService = clientService;
-    }
-
-    @GetMapping("login/oauth2/code/{provider}")
-    public
-}
+//package com.example.demo.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+////@RestController
+//public class OAuth2Controller {
+//    private final OAuth2AuthorizedClientService clientService;
+//
+//    @Autowired
+//    public OAuth2Controller(OAuth2AuthorizedClientService clientService){
+//        this.clientService = clientService;
+//    }
+//
+////    @GetMapping("login/oauth2/code/{provider}")
+////    public
+//}
